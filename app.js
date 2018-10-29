b68d62fa96b6e946c3782f030fe6a1cf7358257c
timliuGH <timsgithub@gmail.com>
2018-10-29 14:48:32 -0700

    add nav bar and page descriptions

--- app.js
+++ app.js
@@ -7,39 +7,46 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');				
 app.set('port', 10376);												
 
+var path = require('path');
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.get('/', function(req, res) {
 	res.render('index');	
 });
 
-app.get('/whosit.html', function(req, res) {
+app.get('/index', function(req, res) {
+	res.render('index');
+});
+
+app.get('/whosit', function(req, res) {
 	res.render('whosit');
 });
 
-app.get('/whysit.html', function(req, res) {
+app.get('/whysit', function(req, res) {
 	res.render('whysit');
 });
 
-app.get('/wheresit.html', function(req, res) {
+app.get('/wheresit', function(req, res) {
 	res.render('wheresit');
 });
 
-app.get('/whatsit.html', function(req, res) {
+app.get('/whatsit', function(req, res) {
 	res.render('whatsit');
 });
 
-app.get('/update.html', function(req, res) {
+app.get('/update', function(req, res) {
 	res.render('update');
 });
 
-app.get('/howsit.html', function(req, res) {
+app.get('/howsit', function(req, res) {
 	res.render('howsit');
 });
 
-app.get('/hasA.html', function(req, res) {
+app.get('/hasA', function(req, res) {
 	res.render('hasA');
 });
 
-app.get('/feelsA.html', function(req, res) {
+app.get('/feelsA', function(req, res) {
 	res.render('feelsA');
 });
 
